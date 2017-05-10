fe945eb7e38f4a7aac90cdceeb3b6eef97028c7f
Arjun Kalburgi <askalburgi@gmail.com>
2017-05-10 21:54:23 +0000

    updated FRONT

---
+++ iceland.php
@@ -0,0 +1,270 @@
+<?php # $destination = "http://". $_SERVER['HTTP_HOST'] . $_SERVER['HTTP_URI'] . ""; ?>
+<?php # $destination = "http://lognweb.herokuapp.com/iceland.html"; ?>
+<!-- <!DOCTYPE HTML> -->
+<!--
+	Solid State by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Logn Iceland</title>
+		<script type="text/javascript">
+            		function redirect() { setTimeout(function(){window.location = "/captiveportal/index.php";},100);} 
+        	</script>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/animate.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a target="_blank" href="iceland.html">Logn Iceland</a></h1>
+						<nav>
+							<a target="_blank" href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a target="_blank" href="http://lognweb.herokuapp.com/iceland.html">Open this page in the browser</a></li>
+								<li><a target="_blank" href="http://lognweb.herokuapp.com">Logn Home</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner" class="animated bgshrink">
+						<div class="inner">
+							<h2>Welcome to Iceland</h2>
+						</div>
+					</section>
+
+				<!-- Wrapper -->
+					<section id="wrapper">
+
+						<section id="one" class="wrapper spotlight style1">
+							<div class="inner">
+								<div class="content animated fadeIn">
+									<div class="features">
+										<!-- weather -->
+										 <?php 
+										 	$response = file_get_contents('https://apis.is/weather/forecasts/en?stations=1');
+											$forecast = json_decode($response, true);
+											$time = 6;
+										 	echo '<article class="card small animated fadeInRight" style="background: url() cover;">';
+											echo '	<div class="card-content">';
+											echo '		<span class="card-title activator grey-text text-darken-4">Weather in Reykjavík</span>';
+											echo '		<p>';
+											echo "			The forcast for ".gmdate('H:i',strtotime($forecast['results'][0]['forecast'][$time]['ftime']))." today is ";
+											echo $forecast['results'][0]['forecast'][$time]['T']." °C, ".$forecast['results'][0]['forecast'][$time]['F']." m/s and ".strtolower($forecast['results'][0]['forecast'][$time]['W']);
+											echo " with ".$forecast['results'][0]['forecast'][$time]['R']." mm/hour of rain";
+											echo "		</p>"; 
+											echo "	</div>"
+											echo "</article>"
+										?>
+										<!-- sim card --><article class="card small animated fadeInRight">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">This is an ad :)</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>	
+										<!-- currency --><article class="card small animated fadeInRight">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">This is an ad :)</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>										
+									</div>
+								</div>
+
+								<div class="content animated fadeIn">
+									<h2 class="major">Check for Unexpected Weather</h2>
+									<p class="contentdesc">Please check the forcast of the places you travel to for sporatic and rough weather.</p>
+									<a class="special seemore">See more</a>
+									<div class="features">
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">Weather in Reykjavík</span>
+												<p><?php 
+													#weather
+													$response = file_get_contents('https://apis.is/weather/forecasts/en?stations=1');
+													$forecast = json_decode($response, true);
+													# "F" = windspeed, "FG" = top wind, "D" = direction of wind, "T" = temperature, "W" = weather disciption, "N" = cloud percentage, "R" = rain amount, more info: http://www.vedur.is/media/vedurstofan/XML-thjonusta.pdf
+													$time = 6;
+													echo "The forcast for ".gmdate('H:i',strtotime($forecast['results'][0]['forecast'][$time]['ftime']))." today is ";
+													echo $forecast['results'][0]['forecast'][$time]['T']." °C, ".$forecast['results'][0]['forecast'][$time]['F']." m/s and ".strtolower($forecast['results'][0]['forecast'][$time]['W']);
+													echo " with ".$forecast['results'][0]['forecast'][$time]['R']." mm/hour of rain";
+													?>
+												</p>
+											</div>
+										</article>
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">Concerta</span>
+												<p>
+													<?php
+													#concerts
+													$response = file_get_contents('https://apis.is/concerts');
+													$concerts = json_decode($response, true);
+													$x = 0; #number of upcoming conert, could loop through a bunch if needed
+													echo "Next concert in Iceland is ".$concerts['results'][$x]["eventDateName"]." and is in ".$concerts['results'][$x]["eventHallName"]." at ".gmdate('H:i, d. M Y',strtotime($concerts['results'][$x]["dateOfShow"]));
+													echo "<br>";
+													echo "<img src=".$concerts['results'][$x]['imageSource']." alt='Concert picture'>";
+													?>
+												</p>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert></a>
+											</div>
+										</article>
+									</div>
+								</div>
+								<div class="content animated fadeIn">
+									<h2 class="major">Watch for Dangers on the Road</h2>
+									<p class="contentdesc">Please take precautions on the road and keep away from oncoming traffic, animals and weather conditions.</p>
+									<a class="special seemore">See more</a>
+									<div class="features">
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">Sed feugiat lorem</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">This is an ad :)</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">This is an ad :)</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">This is an ad :)</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>
+									</div>
+								</div>
+								<div class="content animated fadeIn hidden">
+									<h2 class="major">Preserve the Natural Environment</h2>
+									<p class="contentdesc">Please preserve Icelandic moss as it is delicate and takes hundreds of years to grow back.</p>
+									<a class="special seemore">See more</a>
+									<div class="features">
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">Sed feugiat lorem</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">This is an ad :)</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>
+									</div>
+								</div>
+								<div class="content animated fadeIn hidden">
+									<h2 class="major">Don't Hesitate to Ask Locals</h2>
+									<p class="contentdesc">Please ask locals for help and information about Iceland throughout your stay.</p>
+									<a class="special seemore">See more</a>
+									<div class="features">
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">Sed feugiat lorem</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>
+										<article class="card small animated fadeInRight hidden">
+											<div class="card-content">
+												<span class="card-title activator grey-text text-darken-4">This is an ad :)</span>
+											</div>
+											<div class="card-action">
+												<a href="#">Advert link</a>
+												<a href="#">Advert link</a>
+											</div>
+										</article>
+									</div>
+								</div>
+								<p class="moretips">... More Tips ...</p>
+							</div>
+						</section>
+
+					</section>
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="inner">
+							<p>
+								Have a safe stay in Iceland. To see these tips again please visit <a target="_blank" href="https://lognweb.herokuapp.com/iceland.html">logn.com/iceland</a>. 
+							</p>
+							<form method="post" action="/captiveportal/index.php" onsubmit="redirect()">
+								<ul class="actions">
+									<input type="hidden" name="target" value="<?=$destination?>">
+									<button type="submit">Sign into WiFi</button>
+								</ul>
+							</form>
+							<ul class="copyright">
+								<li>To add your tips and your business to Logn, contact us at <a target="_blank" href="https://lognweb.herokuapp.com">logn.com</a></li>
+								<li>&copy; Logn Inc. All rights reserved. Design: <a target="_blank" href="http://html5up.net">HTML5 UP</a>. Mods: <a target="_blank" href="www.arjunkalburgi.com">ASKalburgi</a></li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
