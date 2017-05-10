0ec7e5650e65a2e3a12cefe8629c0e4fd6140108
gustinn <agust54@gmail.com>
2017-05-10 14:51:15 +0000

    currency test

--- scripts.php
+++ scripts.php
@@ -33,21 +33,26 @@ echo $forecast['results'][0]['forecast'][$time]['T']." °C, ".$forecast['results
 echo "<br>with ".$forecast['results'][0]['forecast'][$time]['R']." mm/hour of rain";
 echo "<br>";
 
-
-#currency ####BROKEN#### USE jQuery or whatevs
+#currency
+echo '<form action="currency.php" method="post">';
 $response = file_get_contents('https://apis.is/currency/lb'); # /currency/{m5,arion,lb}, {m5.is, arionbanki, landsbanki}
 $currency = json_decode($response, true);
 echo '<select id="curr" name="curr">';
 echo '<option value="0">Currency</option>';
-echo '<option value="USD">USD</option>';
-echo '<option value="CAD">CAD</option>';
-echo '<option value="DKK">DKK</option>';
-echo '</select>';
-if($_POST['submit'] && $_POST['submit'] != 0)
-{
-   $curr=$_POST['submit'];
-   echo $curr;
+foreach ($currency['results'] as $nr){
+    $curr = $nr['shortName'];
+    echo '<option value="'.$curr.'">'.$curr.'</option>';
 }
+echo '</select><br>Enter Amount:<input type="text" name="amount"><br>';
+echo '<input type="submit" name="submit" value="Convert Now"></center>';
 
+if(isset($_POST['submit'])){
+	
+	$amount = $_POST['amount'];
+	$curr = $_POST['curr'];
+	$value = 102.5;
+	
+	echo $value*$amount;
+}
 
 ?>
